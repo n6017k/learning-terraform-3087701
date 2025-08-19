b6a5962917887b8d892c949b6954a263349e70bf
nk1752 <71557302+nk1752@users.noreply.github.com>
2025-08-19 12:48:31 -0500

    catch up

--- main.tf
+++ main.tf
@@ -29,35 +29,19 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
-  # insert the 1 required variable here
-  name = "blog-asg"
-  min_size = 1
-  max_size = 2
-  
+
+  name = "blog"
+
+  min_size            = var.asg_min
+  max_size            = var.asg_max
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns = [module.blog_alb.target_group_arns]
-  security_groups = [module.blog_sg.security_group_id]
-
-  image_id = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  launch_template = {
-    name = "blog-launch-template"
-    version = "$Latest"
-    block_device_mappings = [
-      {
-        device_name = "/dev/xvda"
-        ebs = {
-          volume_size = 8
-          volume_type = "gp2"
-          delete_on_termination = true
-        }
-      }
-    ]
-  }
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -95,16 +79,13 @@ module "blog_alb" {
 }
 
 module "blog_sg" {
-  source                = "terraform-aws-modules/security-group/aws"
-  version               = "5.3.0"
-  name                  = "blog"
-  description           = "Allow HTTP and HTTPS traffic"
-  
-  vpc_id                = module.blog_vpc.vpc_id
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-  ingress_rules         = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks   = ["0.0.0.0/0"]
-
-  egress_rules          = ["all-all"]
-  egress_cidr_blocks    = ["0.0.0.0/0"]
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
