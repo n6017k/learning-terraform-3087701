cb200bccf13e316aa3bd0a0f5db5df1b7faef81c
nk1752 <71557302+nk1752@users.noreply.github.com>
2025-08-19 08:48:50 -0500

    vpc_id as string

--- main.tf
+++ main.tf
@@ -35,7 +35,8 @@ module "blog_sg" {
   name                  = "blog_new"
   description           = "Allow HTTP and HTTPS traffic"
   
-  vpc_id                = [data.aws_vpc.default.id]
+  vpc_id                = data.aws_vpc.default.id
+
   ingress_rules         = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks   = ["0.0.0.0/0"]
 
